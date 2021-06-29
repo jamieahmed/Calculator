6a6a3fee3e7343a142f5d8b9f4f099b2f8d111cb
DavidStinson <49245298+DavidStinson@users.noreply.github.com>
2021-06-29 07:57:53 -0500

    touch up

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 /*
- * Instructions: You've been given the HTML template for a JavaScript  
+ * Instructions: You've been given the HTML template for a JavaScript
  * calculator. Implement functionality such that when two numbers are
  * entered in the input fields, pressing one of the 'operator' buttons
  * results in the mathematical calculation for those two numbers.
- * 
- * For example: If the boxes contain 4 and 2, pressing the "+" button should 
+ *
+ * For example: If the boxes contain 4 and 2, pressing the "+" button should
  * display the number 6. If the "-" button is pressed, 2 should be displayed.
  * If the "*" button is pressed, 8 should be displayed. If the "/" button
  * is pressed, 2 should be displayed.
- * 
+ *
  * There are bounuses as well:
  * EASY BONUS:  If the result is odd, change it's color to salmon.
  * MEDIUM BONUS:  Use a single event listener to accomplish this task, rather
