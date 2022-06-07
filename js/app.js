9e6851963ad39bd94c3bd4801bdd7cf81771c83f
Jamie Ahmed <jamieahmed273@gmail.com>
2022-06-07 13:42:52 -0400

    deleted readme

--- js/app.js
+++ js/app.js
@@ -14,3 +14,55 @@
  * MEDIUM BONUS:  Use a single event listener to accomplish this task, rather
  * than 5 of them.
  */
+
+// Pseudocode
+  // Define cached elements
+  // Write functions
+    // add
+    // subtract
+    // multiply
+    // divide
+  // Render result
+
+
+  const input1 = document.getElementById("math-input1")
+  const input2 = document.getElementById("math-input2")
+  const mathResult = document.getElementById("math-result")
+  const plusBtn = document.getElementById("plus")
+  const minusBtn = document.getElementById("minus")
+  const timesBtn = document.getElementById("times")
+  const dividedBtn = document.getElementById("divided")
+  
+  plusBtn.addEventListener('click', function () {
+    // Store result of adding input 1 and input 2
+    let result = parseInt(input1.value) + parseInt(input2.value)
+    // Display the result in HTML (mathResult)
+    mathResult.innerText = result
+    isOdd(result)
+  })
+  
+  minusBtn.addEventListener('click', function () {
+    let result = parseInt(input1.value) - parseInt(input2.value)
+    mathResult.innerText = result
+    isOdd(result)
+  })
+  
+  timesBtn.addEventListener('click', function () {
+    let result = parseInt(input1.value) * parseInt(input2.value)
+    mathResult.innerText = result
+    isOdd(result)
+  })
+  
+  dividedBtn.addEventListener('click', function () {
+    let result = parseInt(input1.value) / parseInt(input2.value)
+    mathResult.innerText = result
+    isOdd(result)
+  })
+  
+  function isOdd(number) {
+    if (number % 2) {
+      mathResult.style.color = 'salmon'
+    } else {
+      mathResult.style.color = 'black'
+    }
+  }
